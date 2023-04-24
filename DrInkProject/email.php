35309a0f755c0d837659cb1669f0d43d9ff91d1a
Riley Cramer <113931790+RileyCramer@users.noreply.github.com>
2023-04-24 16:04:25 -0400

    Add files via upload

---
+++ DrInkProject/email.php
@@ -0,0 +1,27 @@
+
+<!--Riley-->
+if (isset($_POST['submit'])) {
+    $firstname = $_POST['firstname'];
+
+    $lastname = $_POST['lastname'];
+
+    $email = $_POST['email'];
+
+    $comments = $_POST['comments'];
+
+    if(empty($firstname) || empty($lastname) || empty($email)) {
+        $error = 'You are missing one or more fields';
+    } 
+
+    else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = 'Please enter a valid email address';
+    } 
+
+    else {
+        $to = 'riley.cramer10@gmail.com';
+        $headers = "From: $email\r\nReply-To: $email";
+        $subject = "Tatoo"
+        mail($to, $subject, $firstname, $lastname, $headers)
+    }
+    
+}
