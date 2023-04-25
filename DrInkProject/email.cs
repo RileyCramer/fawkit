69155abb54d93a8e9365ba5c6507cef2b92dab9b
Assembly980 <113926702+Assembly980@users.noreply.github.com>
2023-04-25 14:18:32 -0400

    Add files via upload

---
+++ DrInkProject/email.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Net.Mail;
+
+if(firstname == null || lastname == null || email == null)
+{
+    //commit arson
+}
+else
+{
+    MailMessage email = new MailMessage();
+    SmtpClient client = new SmtpClient(riley.cramer10@gmail.com);
+
+    email.From = new MailAddress("snickers31415@gmail.com", "Brandon Twitty");
+    email.Subject = $"{firstname} {lastname} \n Contact Me at: {email}";
+    client.Send(email);
+}
